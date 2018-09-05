362584dd416ee4f8ef6d1cd2130e87802daa0d63
Scott Linderman <scott.linderman@gmail.com>
2018-09-05 13:48:56 -0400

    adding hierarchical nn transitions

--- zimmer/transitions.py
+++ zimmer/transitions.py
@@ -11,7 +11,7 @@ from autograd import grad
 
 from ssm.transitions import _Transitions
 from ssm.util import random_rotation, ensure_args_are_lists, ensure_args_not_none, \
-    logistic, logit, adam_with_convergence_check, one_hot
+    logistic, logit, adam_with_convergence_check, one_hot, relu
 from ssm.preprocessing import interpolate_data
 
 
@@ -66,29 +66,29 @@ class HierarchicalStationaryTransitions(_Transitions):
         log_Ps = np.tile(self.log_Ps[tag][None, :, :], (T-1, 1, 1)) 
         return log_Ps - logsumexp(log_Ps, axis=2, keepdims=True)
             
-    def m_step(self, expectations, datas, inputs, masks, tags, optimizer="adam", num_iters=10, **kwargs):
-        """
-        Use SGD to fit the model parameters
-        """
-        optimizer = dict(sgd=sgd, adam=adam, adam_with_convergence_check=adam_with_convergence_check)[optimizer]
+    # def m_step(self, expectations, datas, inputs, masks, tags, optimizer="adam", num_iters=10, **kwargs):
+    #     """
+    #     Use SGD to fit the model parameters
+    #     """
+    #     optimizer = dict(sgd=sgd, adam=adam, adam_with_convergence_check=adam_with_convergence_check)[optimizer]
         
-        # Define the EM objective
-        def _expected_log_joint(expectations):
-            elbo = 0
-            for data, input, mask, tag, (expected_states, expected_joints) \
-                in zip(datas, inputs, masks, tags, expectations):
-                log_Ps = self.log_transition_matrices(data, input, mask, tag)
-                elbo += np.sum(expected_joints * log_Ps)
-            return elbo
+    #     # Define the EM objective
+    #     def _expected_log_joint(expectations):
+    #         elbo = 0
+    #         for data, input, mask, tag, (expected_states, expected_joints) \
+    #             in zip(datas, inputs, masks, tags, expectations):
+    #             log_Ps = self.log_transition_matrices(data, input, mask, tag)
+    #             elbo += np.sum(expected_joints * log_Ps)
+    #         return elbo
 
-        # define optimization target
-        T = sum([data.shape[0] for data in datas])
-        def _objective(params, itr):
-            self.params = params
-            obj = _expected_log_joint(expectations) + self.log_prior()
-            return -obj / T
+    #     # define optimization target
+    #     T = sum([data.shape[0] for data in datas])
+    #     def _objective(params, itr):
+    #         self.params = params
+    #         obj = _expected_log_joint(expectations) + self.log_prior()
+    #         return -obj / T
 
-        self.params = optimizer(grad(_objective), self.params, num_iters=num_iters, **kwargs)
+    #     self.params = optimizer(grad(_objective), self.params, num_iters=num_iters, **kwargs)
 
 
 class HierarchicalRecurrentTransitions(_Transitions):
@@ -165,29 +165,29 @@ class HierarchicalRecurrentTransitions(_Transitions):
         log_Ps = log_Ps + np.dot(data[:-1], self.Rs[tag].T)[:, None, :]
         return log_Ps - logsumexp(log_Ps, axis=2, keepdims=True)
             
-    def m_step(self, expectations, datas, inputs, masks, tags, optimizer="adam", num_iters=10, **kwargs):
-        """
-        Use SGD to fit the model parameters
-        """
-        optimizer = dict(sgd=sgd, adam=adam, adam_with_convergence_check=adam_with_convergence_check)[optimizer]
+    # def m_step(self, expectations, datas, inputs, masks, tags, optimizer="adam", num_iters=10, **kwargs):
+    #     """
+    #     Use SGD to fit the model parameters
+    #     """
+    #     optimizer = dict(sgd=sgd, adam=adam, adam_with_convergence_check=adam_with_convergence_check)[optimizer]
         
-        # Define the EM objective
-        def _expected_log_joint(expectations):
-            elbo = 0
-            for data, input, mask, tag, (expected_states, expected_joints) \
-                in zip(datas, inputs, masks, tags, expectations):
-                log_Ps = self.log_transition_matrices(data, input, mask, tag)
-                elbo += np.sum(expected_joints * log_Ps)
-            return elbo
+    #     # Define the EM objective
+    #     def _expected_log_joint(expectations):
+    #         elbo = 0
+    #         for data, input, mask, tag, (expected_states, expected_joints) \
+    #             in zip(datas, inputs, masks, tags, expectations):
+    #             log_Ps = self.log_transition_matrices(data, input, mask, tag)
+    #             elbo += np.sum(expected_joints * log_Ps)
+    #         return elbo
 
-        # define optimization target
-        T = sum([data.shape[0] for data in datas])
-        def _objective(params, itr):
-            self.params = params
-            obj = _expected_log_joint(expectations) + self.log_prior()
-            return -obj / T
+    #     # define optimization target
+    #     T = sum([data.shape[0] for data in datas])
+    #     def _objective(params, itr):
+    #         self.params = params
+    #         obj = _expected_log_joint(expectations) + self.log_prior()
+    #         return -obj / T
 
-        self.params = optimizer(grad(_objective), self.params, num_iters=num_iters, **kwargs)
+    #     self.params = optimizer(grad(_objective), self.params, num_iters=num_iters, **kwargs)
 
 
 class HierarchicalRecurrentOnlyTransitions(_Transitions):
@@ -250,26 +250,109 @@ class HierarchicalRecurrentOnlyTransitions(_Transitions):
         log_Ps = np.tile(log_Ps, (1, self.K, 1))                            # expand
         return log_Ps - logsumexp(log_Ps, axis=2, keepdims=True)            # normalize
 
-    def m_step(self, expectations, datas, inputs, masks, tags, optimizer="adam", num_iters=10, **kwargs):
-        """
-        Use SGD to fit the model parameters
-        """
-        optimizer = dict(sgd=sgd, adam=adam, adam_with_convergence_check=adam_with_convergence_check)[optimizer]
+    # def m_step(self, expectations, datas, inputs, masks, tags, optimizer="adam", num_iters=10, **kwargs):
+    #     """
+    #     Use SGD to fit the model parameters
+    #     """
+    #     optimizer = dict(sgd=sgd, adam=adam, adam_with_convergence_check=adam_with_convergence_check)[optimizer]
         
-        # Define the EM objective
-        def _expected_log_joint(expectations):
-            elbo = 0
-            for data, input, mask, tag, (expected_states, expected_joints) \
-                in zip(datas, inputs, masks, tags, expectations):
-                log_Ps = self.log_transition_matrices(data, input, mask, tag)
-                elbo += np.sum(expected_joints * log_Ps)
-            return elbo
+    #     # Define the EM objective
+    #     def _expected_log_joint(expectations):
+    #         elbo = 0
+    #         for data, input, mask, tag, (expected_states, expected_joints) \
+    #             in zip(datas, inputs, masks, tags, expectations):
+    #             log_Ps = self.log_transition_matrices(data, input, mask, tag)
+    #             elbo += np.sum(expected_joints * log_Ps)
+    #         return elbo
 
-        # define optimization target
-        T = sum([data.shape[0] for data in datas])
-        def _objective(params, itr):
-            self.params = params
-            obj = _expected_log_joint(expectations) + self.log_prior()
-            return -obj / T
+    #     # define optimization target
+    #     T = sum([data.shape[0] for data in datas])
+    #     def _objective(params, itr):
+    #         self.params = params
+    #         obj = _expected_log_joint(expectations) + self.log_prior()
+    #         return -obj / T
 
-        self.params = optimizer(grad(_objective), self.params, num_iters=num_iters, **kwargs)
+    #     self.params = optimizer(grad(_objective), self.params, num_iters=num_iters, **kwargs)
+    
+
+class HierarchicalNeuralNetworkRecurrentTransitions(_Transitions):
+    def __init__(self, K, D, G=1, M=0, eta=0.1):
+        super(HierarchicalNeuralNetworkRecurrentTransitions, self).__init__(K, D, M)
+
+        # Global recurrence parameters
+        Ps = .95 * np.eye(K) + .05 * npr.rand(K, K)
+        Ps /= Ps.sum(axis=1, keepdims=True)
+        self.shared_log_Ps = np.log(Ps)
+
+        # Initialize the NN weights
+        layer_sizes = (D + M,) + hidden_layer_sizes + (K,)
+        self.shared_weights = [npr.randn(m, n) for m, n in zip(layer_sizes[:-1], layer_sizes[1:])]
+        self.shared_biases = [npr.randn(n) for n in layer_sizes[1:]]
+
+        nonlinearities = dict(
+            relu=relu,
+            tanh=np.tanh,
+            sigmoid=logistic)
+        self.nonlinearity = nonlinearities[nonlinearity]
+
+        # Per-group parameters
+        self.G = G
+        self.eta = eta
+        self.log_Ps = self.shared_log_Ps + np.sqrt(eta) * npr.randn(G, K, K)
+        self.weights = [[w + np.sqrt(eta) * npr.randn(*w.shape) for w in self.shared_weights] for g in range(G)]
+        self.biases = [[b + np.sqrt(eta) * npr.randn(*b.shape) for b in self.shared_biases] for g in range(G)]
+
+    @property
+    def params(self):
+        return self.shared_log_Ps, self.shared_weights, self.shared_biases, \
+               self.log_Ps, self.weights, self.biases
+    
+    @params.setter
+    def params(self, value):
+        self.shared_log_Ps, self.shared_weights, self.shared_biases, \
+        self.log_Ps, self.weights, self.biases = value
+
+    def permute(self, perm):
+        """
+        Permute the discrete latent states.
+        """
+        self.shared_log_Ps = self.shared_log_Ps[np.ix_(perm, perm)]
+        self.shared_weights[-1] = self.shared_weights[-1][:,perm]
+        self.shared_biases[-1] = self.shared_biases[-1][perm]
+
+        for g in range(self.G):
+            tmp = copy.deepcopy(self.weights[g])
+            tmp[-1] = tmp[-1][:, perm]
+            self.weights[g] = tmp
+
+            tmp = copy.deepcopy(self.biases[g])
+            tmp[-1] = tmp[-1][perm]
+            self.biases[g] = tmp
+
+    def log_prior(self):
+        lp = 0
+        for g in range(self.G):
+            lp += np.sum(norm.logpdf(self.log_Ps[g], self.shared_log_Ps, np.sqrt(self.eta)))
+
+            for w1, w2 in zip(self.shared_weights, self.weights[g]):
+                lp += np.sum(norm.logpdf(w1, w2, np.sqrt(self.eta)))
+
+            for b1, b2 in zip(self.shared_biases, self.biases[g]):
+                lp += np.sum(norm.logpdf(b1, b2, np.sqrt(self.eta)))
+
+        return lp
+            
+    def log_transition_matrices(self, data, input, mask, tag):
+        T, D = data.shape
+
+        # Pass the data and inputs through the neural network 
+        x = np.hstack((data[:-1], input[1:]))
+        for W, b in zip(self.weights[tag], self.biases[tag]):
+            y = np.dot(x, W) + b
+            x = self.nonlinearity(y)
+        
+        # Add the baseline transition biases
+        log_Ps = self.log_Ps[tag][None, :, :] + y[:, None, :]
+
+        # Normalize
+        return log_Ps - logsumexp(log_Ps, axis=2, keepdims=True)
