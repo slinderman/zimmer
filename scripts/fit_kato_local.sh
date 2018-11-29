1f3895bfaa91aa5ac2f0f34708eb0cdb96128ed4
Scott Linderman <scott.linderman@gmail.com>
2018-11-29 16:38:37 -0500

    fixes

--- scripts/fit_kato_local.sh
+++ scripts/fit_kato_local.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
 echo "Fit the Kato dataset with D = " $1 " continuous dims"
 cd ~/Projects/zimmer
-parallel --results logs python experiments/fit_slds.py kato {} $1 --results_dir=results/kato/2018-11-20 --transitions=rbf ::: 2 4 6 8 10 12 14 16 18 20
+parallel --results logs python experiments/fit_slds.py kato {} $1 --results_dir=results/kato/2018-11-20 --transitions=rbf --eta=1e-1 ::: 2 4 6 8 10 12 14 16 18 20 
 echo all processes complete
 python 
  
