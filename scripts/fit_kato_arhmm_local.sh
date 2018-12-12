6a1a10df48609a374eeb18b1bfe2eca3fa0f0039
Scott Linderman <scott.linderman@gmail.com>
2018-12-12 11:54:08 -0500

    fixing bug in arhmm script

--- scripts/fit_kato_arhmm_local.sh
+++ scripts/fit_kato_arhmm_local.sh
@@ -5,7 +5,7 @@ export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
 cd ~/Projects/zimmer
-parallel --results logs python experiments/fit_arhmm.py kato {} --no_hierarchical --results_dir=results/kato/2018-12-1 --transitions=rbf --eta=1e-1 ::: 2 4 6 8 10 12 14 16 18 20 
+parallel --results logs python experiments/fit_arhmm.py kato {} --no_hierarchical --results_dir=results/kato/2018-12-11 --transitions=rbf --eta=1e-1 ::: 2 4 6 8 10 12 14 16 18 20 
 echo all processes complete
 python 
  
